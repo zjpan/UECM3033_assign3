c506035fa8e302259f1c976ebe1221cca835eef9
zjpan <zjpan123@gmail.com>
2016-04-17 00:02:29 +0800

    Added files via upload

--- task1.py
+++ task1.py
@@ -6,8 +6,16 @@ import sympy as sy
 # DO NOT CHANGE THE NAME OF gausslegendre() function
 def gausslegendre(f, a, b, n=20):
     ans = 0
-    # Edit here to implement your code
+    
+    # legendre function in python is used to obtain the nodes and weights for n = 1 to 100
+    x,w = np.polynomial.legendre.leggauss(n)
+    
+    # Transforming the definite integral into the integral from the range -1 to 1
+    y = (b-a)*x/2 + ((b+a)/2)    
 
+    # (b-a)/2 is the jacobian of the transformation
+    ans = ((b-a)/2)*np.dot(w,f(y))
+    
     return ans
 
 if __name__ == "__main__":
